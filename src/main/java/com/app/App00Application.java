ec8c8c038d438bce036b01bf390d84acd17ac877
Fabricio Leonard Leopoldino <fabricioleonard@sistemafieg.net>
2020-01-30 15:57:40 -0300

    Projeto de Teste

---
+++ src/main/java/com/app/App00Application.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App00Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App00Application.class, args);
+	}
+
+}
